01ce4ea082b4fdc12a4b9bef94e1a1b552512e4d
Barayuda <saya.bara@gmail.com>
2018-01-11 17:43:24 +0800

    Finished category component

---
+++ app/components/CategoryModal.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="cat-modal" class="ui small modal">
+    <i class="close icon"></i>
+    <div class="header">
+      Tambah kategori baru
+    </div>
+    <div class="content">
+      <form class="ui form">
+        <div class="field">
+          <label>Nama kategori</label>
+          <input v-model="catName" type"text" placeholder="Masukan nama kategori" />
+        </div>
+        <div class="field">
+          <label>Warna kategori</label>
+          <select v-model="catColor" class="ui simple dropdown">
+            <option value="">Pilih warna</option>
+            <option v-for="color in categoryColor" value="{{color}}">
+              {{color | capitalize}}
+            </option>
+          </select>
+        </div>
+      </form>
+    </div>
+    <div class="actions">
+      <div @click="addCategory" class="ui purple inverted button">
+        Simpan
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import store from '../store'
+
+  export default {
+    data() {
+      return {
+        carName: '',
+        catColor: '',
+        categoryColors: ['red', 'orange', 'yellow', 'olive', 'green',
+          'teal', 'blue', 'violet', 'purple', 'pink', 'brown', 'grey', 'black']
+      }
+    },
+
+    methods: {
+      addCategory() {
+        var newCategory = {}
+        newCategory[this.catName] = this.catColor
+        store.addCategory(newCategory)
+        $('#cat-modal').modal('hide')
+      }
+    },
+
+    events: {
+      'add-category': function() {
+        this.catName = this.catColor = ''
+        $('#cat-modal').modal('show')
+      }
+    }
+  }
+</script>

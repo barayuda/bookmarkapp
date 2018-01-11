e32d5c0eb89e85fc357b5b61cb65da6109b30dd7
Barayuda <saya.bara@gmail.com>
2018-01-11 17:34:19 +0800

    Finished other bookmark component

---
+++ app/components/Bookmark.vue
@@ -0,0 +1,31 @@
+<template>
+  <div @click="openLink" class="item">
+    <div class="content">
+      <i @click.stop="deleteBookmark" class="icon remote right-float"></i>
+      <a class="header">{{title}}</a>
+      <div class="description">
+        {{url}}
+        <a class="ui {{categoryColor}} tiny label right-float">{{category}}</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { shell } from 'electron'
+  import store from '../store'
+
+  export default {
+    props: ['id','title','url','category','categoryColor'],
+
+    methods: {
+      deleteBookmark() {
+        store.deleteBookmark(this.id)
+      },
+
+      openLink() {
+        shell.openExternal(this.url)
+      }
+    }
+  }
+</script>
